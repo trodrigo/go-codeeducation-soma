72e7edc228bfe8f4cee9c67fcfa1e9f66cd42efa
Tarcio Rodrigo <tr.rodrigo@gmail.com>
2020-09-20 18:44:50 -0300

    Test unitario 3

--- src/go-codeeducation-soma/main.go
+++ src/go-codeeducation-soma/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"testing"
 )
 
 func Soma(a, b int) int {
@@ -11,14 +10,4 @@ func Soma(a, b int) int {
 
 func main() {
 	fmt.Println(Soma(5, 5))
-}
-
-func TestSoma(t* testing.T) {
-	result = Soma(2, 2)
-
-	if result != 4{
-		t.Errorf("Soma espera: %d, obitida: %d", 4, result)
-	} else {
-		fmt.Println("Passou")
-	}
 }
